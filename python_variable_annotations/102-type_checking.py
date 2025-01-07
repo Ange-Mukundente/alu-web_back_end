d54ab3f83d7432642a09a76fe3ce90d1d2ddb776
Ange Mukundente <127440144+Ange-Mukundente@users.noreply.github.com>
2025-01-07 10:32:46 +0200

    102-type_checking.py

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -1,20 +1,19 @@
+#!/usr/bin/env python3
+"""Defines Type checking"""
+from typing import List, Tuple
 
-from typing import Tuple, Any, Union, Sequence,List
 
-
-def zoom_array(lst: Sequence[int], factor: Union[int, float] = 2) -> Tuple[Union[int, float]]:
-
-    if isinstance(factor, float):
-        factor = int(factor)
-    zoomed_in: List[Union[int, float]]= [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Type Checking Exercise"""
+    zoomed_in: List = [
         item for item in lst
-        for i in range(factor)
+        for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
