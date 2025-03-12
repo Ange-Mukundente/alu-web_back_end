2bddc511c613cd735d72b6fb71f473ce801a2334
Ange Mukundente <127440144+Ange-Mukundente@users.noreply.github.com>
2025-03-12 20:32:30 +0200

    server.js

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,14 +1,13 @@
-/* eslint-disable no-console */
 import express from 'express';
-import router from './routes';
+import controllerRouting from './routes/index';
 
 const app = express();
+const port = 1245;
 
-app.use(express.json());
+controllerRouting(app);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-
-app.listen(1245, () => console.log('Server is running on port 1245'));
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
 
 export default app;
